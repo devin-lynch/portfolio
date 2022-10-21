27cf114aa3bda9ebd276c18c710991d421ed22cb
devin-lynch <devin.l.lynch@gmail.com>
2022-10-21 18:50:06 -0500

    Fix typo

--- components/navBar.jsx
+++ components/navBar.jsx
@@ -1,11 +1,11 @@
 // import build in link from next
-import Link from 'next/Link'
+import Link from 'next/link'
 
 export default function NavBar() {
     return (
         <nav>
             <Link href='/'>Home</Link>
-            
+
             <Link href='/about'>About Me</Link>
         </nav>
     )
