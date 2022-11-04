b09128cdc08957e7a14b25ce1a59279fe106e502
devin-lynch <devin.l.lynch@gmail.com>
2022-11-04 18:12:53 -0500

    Stub skills

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -2,6 +2,10 @@ export default function Contact() {
     return (
         <div>
             Contact Me
+
+            <div>Resume</div>
+            <div>LinkedIn</div>
+            <div>Github</div>
         </div>
     )
 }
--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,19 +1,137 @@
+import Image from 'next/image'
+
 export default function Skills() {
     return (
         <div>
-            <div>JavaScript</div>
-            <div>MongoDB</div>
-            <div>Express.js</div>
-            <div>React</div>
-            <div>Node.js</div>
-            <div>Python</div>
-            <div>Django</div>
-            <div>SQL</div>
-            <div>Tailwind</div>
-            <div>CSS</div>
-            <div>HTML</div>
-            <div>Git</div>
-            <div>Github</div>
+            <div>
+                JavaScript
+                <Image
+                    src='/js.png'
+                    alt='JavaScript logo'
+                    width='90'
+                    height='90'
+                />
+            </div>
+
+            <div>
+                MongoDB
+                <Image
+                    src='/mongodb.png'
+                    alt='MongoDB logo'
+                    width='75'
+                    height='75'
+                />
+            </div>
+
+            <div>
+                Express.js
+                <Image
+                    src='/express.png'
+                    alt='Express.js logo'
+                    width='130'
+                    height='75'
+                />
+            </div>
+
+            <div>
+                React
+                <Image
+                    src='/react.png'
+                    alt='React logo'
+                    width='75'
+                    height='75'
+                />
+            </div>
+
+            <div>
+                Node.js
+                <Image
+                    src='/node.png'
+                    alt='Node.js logo'
+                    width='75'
+                    height='75'
+                />
+            </div>
+
+            <div>
+                Python
+                <Image
+                    src='/python.png'
+                    alt='Python logo'
+                    width='75'
+                    height='75'
+                />
+            </div>
+
+            <div>
+                Django
+                <Image
+                    src='/django.png'
+                    alt='Django logo'
+                    width='75'
+                    height='75'
+                />
+            </div>
+
+            <div>
+                PostgreSQL
+                <Image
+                    src='/postgres.png'
+                    alt='PostgreSQL logo'
+                    width='115'
+                    height='115'
+                />
+            </div>
+
+            <div>
+                Tailwind
+                <Image
+                    src='/tailwind.png'
+                    alt='Tailwind logo'
+                    width='110'
+                    height='110'
+                />
+            </div>
+
+            <div>
+                CSS
+                <Image
+                    src='/css.png'
+                    alt='CSS logo'
+                    width='62'
+                    height='90'
+                />
+            </div>
+
+            <div>
+                HTML
+                <Image
+                    src='/html.png'
+                    alt='HTML logo'
+                    width='100'
+                    height='100'
+                />
+            </div>
+
+            <div>
+                Git
+                <Image
+                    src='/git.png'
+                    alt='Git logo'
+                    width='75'
+                    height='75'
+                />
+            </div>
+
+            <div>
+                Github
+                <Image
+                    src='/github.png'
+                    alt='Github logo'
+                    width='75'
+                    height='75'
+                />
+            </div>
         </div>
     )
 }
