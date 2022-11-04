8b534c007242d75f994e81dbc8d43446b4fe1671
devin-lynch <devin.l.lynch@gmail.com>
2022-11-04 17:14:09 -0500

    Change components

--- components/navBar.jsx
+++ components/navBar.jsx
@@ -4,11 +4,11 @@ import Link from 'next/link'
 export default function Navbar() {
     return (
         <nav>
-            <Link href='/'>Home</Link>
+            <Link href='/'>Home</Link>{' | '}
 
-            <Link href='/about'>About Me</Link>
+            <Link href='/about'>About Me</Link>{' | '}
 
-            <Link href='/projects'>Projects</Link>
+            <Link href='/projects'>Projects</Link>{' | '}
 
             <Link href='/contact'>Contact</Link>
         </nav>
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import About from '../components/about'
+import Contact from '../components/contact'
+import Projects from '../components/projects'
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -12,6 +16,12 @@ export default function Home() {
       </Head>
       {/* useRef + scrolling */}
       <h1>My Portfolio</h1>
+
+      <About />
+
+      <Projects />
+
+      <Contact />
     </div>
   )
 }
