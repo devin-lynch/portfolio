703ff06c40872464df8224eaa76ec75393f45a5b
devin-lynch <devin.l.lynch@gmail.com>
2022-11-04 19:04:39 -0500

    Further debugging vercel error

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,10 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-import About from '../components/about'
+import About from '../components/About'
 import Skills from '../components/Skills'
-import Projects from '../components/projects'
-import Contact from '../components/contact'
+import Projects from '../components/Projects'
+import Contact from '../components/Contact'
 
 export default function Home() {
   return (
