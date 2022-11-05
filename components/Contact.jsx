0d33be4b7acf5a4fe0ffe4d30658bbe6d0cad8c0
devin-lynch <devin.l.lynch@gmail.com>
2022-11-04 19:14:08 -0500

    Add contact links

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,11 +1,34 @@
+import Image from 'next/image'
+
 export default function Contact() {
     return (
         <div>
             Contact Me
 
-            <div>Resume</div>
-            <div>LinkedIn</div>
-            <div>Github</div>
+            <div>Resume</div> {/* check on removing phone number from resume before linking */}
+
+            <div>
+                <a href='https://www.linkedin.com/in/lynch-devin/'>
+                    <Image
+                        src='/linkedin.png'
+                        alt='LinkedIn logo'
+                        height='75'
+                        width='75'
+                    />
+                </a>
+            </div>
+
+            <div>
+                <a href='https://github.com/devin-lynch'>
+                    <Image
+                        src='/githubwhite.png'
+                        alt='Github logo'
+                        height='75'
+                        width='75'
+                    />
+                </a>
+            </div>
+
         </div>
     )
 }
