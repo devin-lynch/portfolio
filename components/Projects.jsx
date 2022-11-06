373aa8804a506ee4e3a28a7392149bb6f99489ec
devin-lynch <devin.l.lynch@gmail.com>
2022-11-06 16:11:18 -0600

    Format projects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -6,55 +6,63 @@ export default function Projects() {
             <p className='mt-6'>My Projects</p>
 
             <div className='text-center mt-3'>
-                <p className='italic text-3xl'><a href='https://dreamplanner-dl.netlify.app/'>DreamPlanner</a></p>
-                <a href='https://dreamplanner-dl.netlify.app/'><img src='https://i.imgur.com/eyliRfW.png' className='project' /></a>
-                <a href='https://github.com/devin-lynch/dreamplanner-client'>
-                    <Image
-                        src='/github.png'
-                        alt='Github logo'
-                        height='50'
-                        width='50'
-                    />
-                </a>
+                <div className='flex items-center gap-2'>
+                    <a href='https://github.com/devin-lynch/dreamplanner-client'>
+                        <Image
+                            src='/github.png'
+                            alt='Github logo'
+                            height='50'
+                            width='50'
+                        />
+                    </a>
+                    <a href='https://dreamplanner-dl.netlify.app/'><img src='https://i.imgur.com/eyliRfW.png' className='project' /></a>
+                    <p className=''><span><p className='italic text-3xl'><a href='https://dreamplanner-dl.netlify.app/'>DreamPlanner</a></p></span>A vacation expense/budget planner with unique user profiles to track destinations <br></br> Tech stack: React, Python, Django, PostgreSQL</p>
+                </div>
             </div>
 
-            <div className='text-center mt-5'>
-                <p className='italic text-3xl'><a href='https://silver-beignet-215e62.netlify.app/'>Pod-Cats</a></p>
-                <a href='https://silver-beignet-215e62.netlify.app/'><img src='https://i.imgur.com/ZMEAjd0.png' className='project' /></a>
-                <a href='https://github.com/devin-lynch/pod-cats-client'>
-                    <Image
-                        src='/github.png'
-                        alt='Github logo'
-                        height='50'
-                        width='50'
-                    />
-                </a>
+            <div className='text-center mt-10'>
+                <div className='flex items-center gap-2'>
+                    <p><span><p className='italic text-3xl'><a href='https://silver-beignet-215e62.netlify.app/'>Pod-Cats</a></p></span>A cat-lover’s social media app to browse, save, and add comments to cats <br></br> Tech stack: MongoDB, Express, React, Node</p>
+                    <a href='https://silver-beignet-215e62.netlify.app/'><img src='https://i.imgur.com/ZMEAjd0.png' className='project' /></a>
+                    <a href='https://github.com/devin-lynch/pod-cats-client'>
+                        <Image
+                            src='/github.png'
+                            alt='Github logo'
+                            height='50'
+                            width='50'
+                        />
+                    </a>
+                </div>
             </div>
 
-            <div className='text-center mt-5'>
-                <p className='italic text-3xl'><a href='https://nba-app-devin-lynch.koyeb.app/'>NBA App</a></p>
-                <a href='https://nba-app-devin-lynch.koyeb.app/'><img src='https://i.imgur.com/u698Ork.png' className='project' /></a>
-                <a href='https://github.com/devin-lynch/proj-2-nba-app'>
-                    <Image
-                        src='/github.png'
-                        alt='Github logo'
-                        height='50'
-                        width='50'
-                    />
-                </a>
+            <div className='text-center mt-10'>
+                <div  className='flex items-center gap-2'>
+                    <a href='https://github.com/devin-lynch/proj-2-nba-app'>
+                        <Image
+                            src='/github.png'
+                            alt='Github logo'
+                            height='50'
+                            width='50'
+                        />
+                    </a>
+                    <a href='https://nba-app-devin-lynch.koyeb.app/'><img src='https://i.imgur.com/u698Ork.png' className='project' /></a>
+                    <p><span><p className='italic text-3xl'><a href='https://nba-app-devin-lynch.koyeb.app/'>NBA App</a></p></span>An NBA App to browse players/teams and save favorites to add/edit comments on unique user profiles <br></br> Tech stack: Node, Express, PostgreSQL, Sequelize, EJS</p>
+                </div>
             </div>
             
-            <div className='text-center mt-5'>
-                <p className='italic text-3xl'><a href='https://devin-lynch.github.io/PokeDOM-Battle/'>PokeDOM Battle</a></p>
-                <a href='https://devin-lynch.github.io/PokeDOM-Battle/'><img src='https://i.imgur.com/RDEDulR.png' className='project' /></a>
-                <a href='https://github.com/devin-lynch/PokeDOM-Battle'>
-                    <Image
-                        src='/github.png'
-                        alt='Github logo'
-                        height='50'
-                        width='50'
-                    />
-                </a>
+            <div className='text-center mt-10'>
+                <div className='flex items-center gap-2'>
+                    <p><span><p className='italic text-3xl'><a href='https://devin-lynch.github.io/PokeDOM-Battle/'>PokeDOM Battle</a></p></span>A Pokemon battle simulation using DOM manipulation <br></br> Tech stack: JavaScript, HTML, CSS</p>
+                    <a href='https://devin-lynch.github.io/PokeDOM-Battle/'><img src='https://i.imgur.com/RDEDulR.png' className='project' /></a>
+                    <a href='https://github.com/devin-lynch/PokeDOM-Battle'>
+                        <Image
+                            src='/github.png'
+                            alt='Github logo'
+                            height='50'
+                            width='50'
+                        />
+                    </a>
+                </div>
             </div>
             
         </div>
