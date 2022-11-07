abd5f45452b9b42fac6690b2c789d01b5e742277
devin-lynch <devin.l.lynch@gmail.com>
2022-11-06 19:00:17 -0600

    Adjust font

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,7 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Poiret+One&display=swap" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />  
       </Head>
       {/* useRef + scrolling */}
       <p className='text-xl mt-5 text-4xl italic font-bold'>Devin Lynch</p>
